f8794acd1ddd65a102491be2ee9042cb6528a437
Albert Julius Liu <ajul197@gmail.com>
2014-02-18 11:57:51 -0500

    add timing to savetest.py

--- savetest.py
+++ savetest.py
@@ -2,7 +2,24 @@ import os
 import re
 import collections
 import pyradox.txt
+import time
 
-savetree = pyradox.txt.parseFile('in/testsave.eu4')
-print(savetree["player"])
+def parseFile(filename):
+    """Parse a single file and return a Tree."""
+    f = open(filename)
+    fileLines = f.readlines()
+    f.close()
+    startTime = time.clock()
+    print('Lexing file %s.' % filename)
+    tokenData = list(pyradox.txt.lex(fileLines, filename))
+    lexTime = time.clock()
+    print('Lexed in %ss.' % (lexTime - startTime))
+    print('Parsing file %s.' % filename)
+    result = pyradox.txt.parse(tokenData, filename)
+    parseTime = time.clock()
+    print('Parsed in %ss.' % (parseTime - lexTime))
+    return result
+
+savetree = parseFile('in/testsave.eu4')
+print(savetree["checksum"])
 
