172c447a64eb4886c14b09a83e6e56fc25f3006c
ajul <ajul1987@gmail.com>
2016-10-02 13:07:02 -0400

    don't warn YML on comment lines

--- pyradox/yml.py
+++ pyradox/yml.py
@@ -52,6 +52,8 @@ def parseFile(filename):
     parseLines(lines, filename)
     
 def parseLine(lineNumber, line, filename):
+    comment = re.match(r'\s*#.*', line)
+    if comment is not None: return
     m = re.match(r'\s*([\w-]+):\d?\s*("*)(.*)(\2)\s*', line)
     if m is not None:
         key, value = m.group(1, 3)
