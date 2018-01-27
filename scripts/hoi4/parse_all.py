1bd72f16c1b0c7eb30cb4b4b3d517b5213cd2900
ajul <ajul1987@gmail.com>
2018-01-27 15:17:22 -0500

    add crude timing to parse_all

--- scripts/hoi4/parse_all.py
+++ scripts/hoi4/parse_all.py
@@ -3,12 +3,16 @@ import os
 import re
 import sys
 import pyradox
+import time
 
 def parse_walk(dirname, verbose=False):
     """Given a directory, recursively iterate over the content of the .txt files in that directory as Trees"""
     
 skip = '\\\\(map|wiki|interface|pdx_launcher|previewer_assets)\\\\'
 
+start_time = time.clock()
+
+file_count = 0
 
 dirname = os.path.join(pyradox.get_game_directory('HoI4'))
 for root, dirs, files in os.walk(dirname):
@@ -19,5 +23,9 @@ for root, dirs, files in os.walk(dirname):
         if ext == ".txt" and not re.search(skip, fullpath):
             try:
                 pyradox.parse_file(fullpath)
+                file_count += 1
             except:
                 print(sys.exc_info())
+end_time = time.clock()
+
+print("Parsed %d files in %0.1fs" % (file_count, end_time - start_time))
