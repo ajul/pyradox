d1459b48a0eab57616bbe075d0858d35d5325ea9
ajul <ajul1987@gmail.com>
2018-01-27 01:51:37 -0500

    start reorganization into submodules

--- pyradox/datatype/color.py
+++ pyradox/datatype/color.py
@@ -1,11 +1,6 @@
 import warnings
 
-class ValueWarning(Warning):
-    def __init__(self, message):
-        self.message = message
-
-    def __str__(self):
-        return self.message
+from pyradox.warnings import ValueWarning
 
 class Color():
     """
