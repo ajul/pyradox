4390c9af18807de770329d455ff699485f51e6b5
ajul <ajul1987@gmail.com>
2018-03-12 22:18:08 -0400

    stellaris age script

--- scripts/stellaris/old_age.py
+++ scripts/stellaris/old_age.py
@@ -1,8 +1,22 @@
 result = 0.0
 survival = 1.0
-for year in range(1000):
-    for month in range(12):
-        survival *= (1.0 - 1.0e-4 * (year + month / 12.0))
-        result += survival / 12.0
 
-print(result)
+year = 0
+
+median = False
+
+while survival > 0.0:
+    if year <= 20:
+        death_chance = year * 0.002
+    else:
+        death_chance = 20 * 0.002 + (year - 20) * 0.02 # in addition?
+    death_chance = min(death_chance, 1.0)
+    for month in range(12):
+        survival *= (1.0 - death_chance)
+        if survival < 0.5 and not median:
+            median = True
+            print(year, month)
+        result += survival
+    year += 1
+
+print(int(result // 12), round(result % 12))
